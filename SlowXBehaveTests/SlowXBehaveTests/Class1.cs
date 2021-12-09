a143063c28744d11b32e6f08d049f82bfc02291e
Urs Enzler <u.enzler@calitime.ch>
2021-12-09 15:10:09 +0100

    repro

---
+++ SlowXBehaveTests/SlowXBehaveTests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Xbehave;
+using Xunit;
+
+namespace SlowXBehaveTests;
+
+public class Scenarios
+{
+    [Scenario]
+    public void Scenario()
+    {
+        for (var i = 0; i < 1000; i++)
+        {
+            $"step {i}".x(() => { });
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Theory(int i)
+    {
+
+    }
+
+    public static IEnumerable<object[]> Data()
+    {
+        var array = new List<object[]>(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            array.Add(new[] {(object)i});
+        }
+
+        return array;
+    }
+}
